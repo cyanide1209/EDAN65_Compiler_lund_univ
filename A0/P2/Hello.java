55ae0c4d8fe3bfa7510578de60bfe32f221814f6
Sayan Jain <sayan.jain@gmail.com>
2023-09-03 13:43:27 +0200

    A0, p1, p2

---
+++ A0/P2/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+        public static void main(String[] args) {
+                int x = 5;
+                System.out.println("Hello!");
+                x=7;
+                
+        }
+}
--- A0/README.md
+++ A0/README.md
@@ -1,4 +1,4 @@
 # Assignment A0
 
 
-
+There will be a branch for each set of questions and then within that all the files relating to the answer for those set of questions 
