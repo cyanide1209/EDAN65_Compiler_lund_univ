3fd2ad0a23c6956930c5edc8c576cfc5e2e94248
Sayan Jain <sayan.jain@gmail.com>
2023-09-05 21:04:13 +0200

    3.3 pt 3 after a errors in ParseTests.java

--- A1/MiniS/src/test/lang/ParseTests.java
+++ A1/MiniS/src/test/lang/ParseTests.java
@@ -12,35 +12,19 @@ public class ParseTests {
 	/** Directory where the test input files are stored. */
 	private static final File TEST_DIRECTORY = new File("testfiles");
 
-	@Test public void identifier() {
-		Util.testValidSyntax(TEST_DIRECTORY, "identifier.in");
+	@Test public void exampleprogram() {
+		Util.testValidSyntax(TEST_DIRECTORY, "exampleprogram.in");
 	}
 
-	@Test public void let() {
-		Util.testValidSyntax(TEST_DIRECTORY, "let.in");
+	@Test public void parseError() {
+		Util.testSyntaxError(TEST_DIRECTORY, "parseError.in");
 	}
 
-	@Test public void numerical() {
-		Util.testValidSyntax(TEST_DIRECTORY, "numerical.in");
+	@Test public void scanError() {
+		Util.testSyntaxError(TEST_DIRECTORY, "scanError.in");
 	}
 
-	@Test public void product() {
-		Util.testValidSyntax(TEST_DIRECTORY, "product.in");
-	}
-
-	@Test public void error() {
-		Util.testSyntaxError(TEST_DIRECTORY, "error.in");
-	}
-
-	@Test public void error2() {
-		Util.testSyntaxError(TEST_DIRECTORY, "error2.in");
-	}
-
-	@Test public void error3() {
-		Util.testSyntaxError(TEST_DIRECTORY, "error3.in");
-	}
-
-	@Test public void myexample2() {
-		Util.testSyntaxError(TEST_DIRECTORY, "myexample2.in");
+	@Test public void shortestprogram() {
+		Util.testValidSyntax(TEST_DIRECTORY, "shortestprogram.in");
 	}
 }
