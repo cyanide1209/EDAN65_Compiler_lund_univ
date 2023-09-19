11091d9927834d9d06f626dd4ad097a62e2746d4
@gabriel.bernini.1106 <gabriel.bernini.1106@student.lu.se>
2023-09-19 19:56:52 +0200

    A3.3

--- A3/A3-MinTree-solution/src/test/lang/TestMinValue.java
+++ A3/A3-MinTree-solution/src/test/lang/TestMinValue.java
@@ -58,6 +58,18 @@ public class TestMinValue {
 	@Test
 	public void nbrOfMinValues() {
 		Leaf l1 = new Leaf(1);
+		Leaf l2 = new Leaf(1);
+		Leaf l3 = new Leaf(3);
+		Pair p1 = new Pair(l2, l3);
+		Pair p2 = new Pair(l1, p1);
+		Program p = new Program(p2);
+		
+		assertEquals(2, p.nbrOfMinValues());
+	}
+
+	@Test
+	public void nbrOfMinValuesOvr1() {
+		Leaf l1 = new Leaf(1);
 		Leaf l2 = new Leaf(2);
 		Leaf l3 = new Leaf(3);
 		Pair p1 = new Pair(l2, l3);
