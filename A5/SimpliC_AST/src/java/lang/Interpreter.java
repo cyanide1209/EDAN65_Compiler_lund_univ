afe7704c46ef9fd03865b9725d870ec64d961ca6
Sayan Jain <sayan.jain@gmail.com>
2023-10-03 16:46:22 +0200

    changes

--- A5/SimpliC_AST/src/java/lang/Interpreter.java
+++ A5/SimpliC_AST/src/java/lang/Interpreter.java
@@ -12,7 +12,52 @@ import lang.ast.LangScanner;
 import lang.ast.ErrorMessage;
 
 public class Interpreter {
-    public static void main(String[] args) {
-        
-    }
+    public static Object DrAST_root_node; //Enable debugging with DrAST
+    
+	public static void main(String[] args) {
+		try {
+			if (args.length != 1) {
+				System.err.println(
+						"You must specify a source file on the command line!");
+				printUsage();
+				System.exit(1);
+				return;
+			}
+
+			String filename = args[0];
+			LangScanner scanner = new LangScanner(new FileReader(filename));
+			LangParser parser = new LangParser();
+			Program program = (Program) parser.parse(scanner);
+            DrAST_root_node = program; //Enable debugging with DrAST
+			System.out.println(program.dumpTree());
+			System.out.println(String.valueOf(MSN.maximum(program)));
+			program.prettyPrint(System.out);
+
+			if (!program.errors().isEmpty()) {
+				System.err.println();
+				System.err.println("Errors: ");
+				for (ErrorMessage e: program.errors()) {
+					System.err.println("- " + e);
+				}
+				System.exit(1);
+			}
+			System.out.println("No errors found.");
+            program.eval();
+            DrAST_root_node = program; 
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found!");
+			System.exit(1);
+		} catch (IOException e) {
+			e.printStackTrace(System.err);
+			System.exit(1);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+    private static void printUsage() {
+		System.err.println("Usage: DumpTree FILE");
+		System.err.println("  where FILE is the file to be parsed");
+	}
 }
